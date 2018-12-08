cf99539f7f9180a6005db7cda4121380eaa59f9b
Daniel Mueller <deso@posteo.net>
2018-12-08 11:50:55 -0800

    Deny three more lints
    With this change we add three more lints to the 'deny' group. The lints
    are believed to be stable in the sense that future versions of Rust
    should not be stricter about their enforcement.

--- src/lib.rs
+++ src/lib.rs
@@ -19,8 +19,11 @@
 
 #![no_std]
 #![deny(
+  missing_copy_implementations,
   missing_debug_implementations,
   missing_docs,
+  trivial_casts,
+  trivial_numeric_casts,
   unstable_features,
   unused_import_braces,
   unused_qualifications,
